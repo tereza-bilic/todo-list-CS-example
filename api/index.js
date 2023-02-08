0a102e87be745b0722cf609165d7474263cd4c04
Nino <11751305+nogicn@users.noreply.github.com>
2023-02-08 21:59:24 +0100

    dodaj cors

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ var app = express();//takes us to the root(/) URL
 var bodyParser = require('body-parser');
 //app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// allow cors
+// allow cors in express
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
