258c22f65ecd2aa9e34ae01bcc36acc49a696b2d
Tereza <tereza.bilic@productive.io>
2023-02-08 22:06:19 +0100

    front: add login and registration form

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
 import './App.css';
+import RegistrationForm from './components/RegistrationForm/RegistrationForm';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginForm from './components/LoginForm/LoginForm';
+
+const router = createBrowserRouter([
+  {
+    path: "/register",
+    element: <RegistrationForm />,
+  },
+  {
+    path: "/",
+    element: <LoginForm />,
+  }
+]);
+
 
 function App() {
   return (
     <div className="App">
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,81 @@
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import axios from 'axios';
+
+export default function LoginForm() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+    axios.post('http://localhost:4000/login', {
+      username: data.get('email'),
+      password: data.get('password')
+    })
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+            </Grid>
+            <Grid item>
+              <Link href="/register" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
